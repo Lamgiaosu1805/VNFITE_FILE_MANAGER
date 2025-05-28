096a4db5fa735b0c8ff944edadd23687a2076ef0
Lamgs <nghiemkhaclam185.mta@gmail.com>
2025-05-28 14:29:44 +0700

    update

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -92,7 +92,7 @@ const FileController = {
         try {
             const {idFile} = req.params
             const file = await FileUploadModel.findById(idFile)
-            const imagePath = path.resolve('/var/www/', file.url);
+            const imagePath = path.resolve('/var/www', file.url);
             if (!fs.existsSync(imagePath)) {
                 return res.status(404).send('Image not found');
             }
